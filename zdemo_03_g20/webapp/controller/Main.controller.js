f3ff8baa6dffc860adc69e46bd8b55570ef9c34c
Youngbum Kwon <youngeyaa@yonsei.ac.kr>
2026-02-23 02:46:57 +0000

    [feat] : Panel Binding, Editable

--- zdemo_03_g20/webapp/controller/Main.controller.js
+++ zdemo_03_g20/webapp/controller/Main.controller.js
@@ -5,6 +5,13 @@ sap.ui.define([
 
     return Controller.extend("code.zdemo03g20.controller.Main", {
         onInit() {
+
+            let oData = {
+                isEdit: false
+            };
+
+            let oModel = new sap.ui.model.json.JSONModel(oData);
+            this.getView().setModel(oModel, "mode");
         },
 
         onSearch(){
@@ -13,11 +20,13 @@ sap.ui.define([
             let cityFrom = this.byId("idFrominput").getValue();
             if (cityFrom){
                 aFilters.push(new sap.ui.model.Filter("Cityfrom", sap.ui.model.FilterOperator.Contains, cityFrom))
+                // new spa.ui.model.Filter("어느 항목에서 찾을지", "어떻게 찾을지", 입력한 값)
+                // Cityfrom 과 같은 컬럼명이 정확히 기억나지 않을 경우 segw 를 참고할 것.
             };
 
             let cityTo = this.byId("idToinput").getValue();
             if(cityTo){
-                aFilters.push(new sap.ui.model.Filter("CityTo", sap.ui.model.FilterOperator.Contains, cityTo))
+                aFilters.push(new sap.ui.model.Filter("Cityto", sap.ui.model.FilterOperator.Contains, cityTo))
             };
 
             let oTable = this.byId("idInfoTable");
@@ -35,6 +44,33 @@ sap.ui.define([
                     ]
                 })
             });
+        },
+
+        onSelectionChange (oEvent){
+            let oSelectedItem = oEvent.getParameter("listItem");
+            let oContext = oSelectedItem.getBindingContext();
+
+            if(!oContext){
+                return;
+            };
+
+            let sPath = oContext.getPath();
+            let oPanel = this.byId("panInfo");
+            oPanel.bindElement(sPath);
+
+            let oModel = this.getView().getModel("mode");
+            oModel.setProperty("/isEdit", true);
+        },
+
+        onReset(){
+            let oPanel = this.byId("panInfo");
+            oPanel.unbindElement();
+
+            
+            let oModel = this.getView().getModel("mode");
+            oModel.setProperty("/isEdit", false);
+
         }
+
     });
 });
