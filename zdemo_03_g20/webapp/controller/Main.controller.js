e9ef2622f375d2b19f542b33154cf0b96cc1d479
Youngbum Kwon <youngeyaa@yonsei.ac.kr>
2026-02-23 01:28:31 +0000

    [feat] : 출발/도착 도시 검색 기능 및 결과 테이블 구현

--- zdemo_03_g20/webapp/controller/Main.controller.js
+++ zdemo_03_g20/webapp/controller/Main.controller.js
@@ -5,6 +5,36 @@ sap.ui.define([
 
     return Controller.extend("code.zdemo03g20.controller.Main", {
         onInit() {
+        },
+
+        onSearch(){
+
+            let aFilters = [];
+            let cityFrom = this.byId("idFrominput").getValue();
+            if (cityFrom){
+                aFilters.push(new sap.ui.model.Filter("Cityfrom", sap.ui.model.FilterOperator.Contains, cityFrom))
+            };
+
+            let cityTo = this.byId("idToinput").getValue();
+            if(cityTo){
+                aFilters.push(new sap.ui.model.Filter("CityTo", sap.ui.model.FilterOperator.Contains, cityTo))
+            };
+
+            let oTable = this.byId("idInfoTable");
+            oTable.bindItems({
+                path: "/ConnectSet",
+                filters: aFilters,
+                template: new sap.m.ColumnListItem({
+                    cells: [
+                        new sap.m.Text({text: "{Carrid}"}),
+                        new sap.m.Text({text: "{Connid}"}),
+                        new sap.m.Text({text: "{Cityfrom}"}),
+                        new sap.m.Text({text: "{Airpfrom}"}),
+                        new sap.m.Text({text: "{Cityto}"}),
+                        new sap.m.Text({text: "{Airpto}"}),
+                    ]
+                })
+            });
         }
     });
 });
