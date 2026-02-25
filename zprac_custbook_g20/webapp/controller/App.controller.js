214bb52c0649dc80b12523d1214c96450bb4d688
Youngbum Kwon <youngeyaa@yonsei.ac.kr>
2026-02-25 04:12:32 +0000

    [new] : zprac_custbook_g20

---
+++ zprac_custbook_g20/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("code.zpraccustbookg20.controller.App", {
+      onInit() {
+      }
+  });
+});
