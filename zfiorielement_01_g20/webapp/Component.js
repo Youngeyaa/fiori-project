8ad7193076cccf0346ccb9c2d7560b1efe18ca7c
Youngbum Kwon <youngeyaa@yonsei.ac.kr>
2026-02-26 04:06:06 +0000

    [new] : zfiorielements

---
+++ zfiorielement_01_g20/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("code.zfiorielement01g20.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
