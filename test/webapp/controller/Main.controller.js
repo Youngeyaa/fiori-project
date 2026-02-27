ad2de0232fcb9d40e7a1ad4cd15a200627d91f73
Youngeyaa <dragon8566@outlook.com>
2026-02-27 14:17:17 +0900

    [test] : from windoes vs

---
+++ test/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("code.test.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ test/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"code/test/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
