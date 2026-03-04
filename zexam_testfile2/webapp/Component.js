e647a08892488317fd62f0b1a631deeaae43fd24
Youngeyaa <dragon8566@outlook.com>
2026-03-04 10:56:48 +0900

    [test] : tests

---
+++ zexam_testfile2/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("code.zexamtestfile2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
